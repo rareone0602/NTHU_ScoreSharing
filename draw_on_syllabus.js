8ef9a4f3b3f7ebde98291ac3107792c9a4415918
b0w1d <61a5t0@gmail.com>
2019-03-07 21:24:21 +0800

    Fix error

--- draw_on_syllabus.js
+++ draw_on_syllabus.js
@@ -10,24 +10,16 @@ async function draw_on_syllabus() {
   let prof_names = Array.from(
     document.body.querySelectorAll('.class3')[5].childNodes
   ).filter((a, i) => ~i&1).map(t => t.data.match(/[^\s(]+/)[0]).sort();
-
   if (course_name.length == 0 || prof_names.length == 0) return;
-
   let query_key = `${course_name}:${prof_names.join(';')}`;
-  let course_ids = await (async() => {
-    const uri = `https://nthuscoresharing.firebaseio.com/queries/${query_key}.json`;
-    const response = await fetch(uri);
-    const json = await response.json();
-    return json ? Object.keys(json).map(s => s.replace(/\+/g, ' ')) : [];
-  })();
 
   let data = await (async() => {
     const uri = `https://nthuscoresharing.herokuapp.com/query?user_id=${user_id}&query_key=${query_key}`;
     const response = await fetch(uri);
-    return response.json();
+    const text = await response.text();
+    try { return JSON.parse(text); }
+    catch(err) { return null; }
   })();
 
-  if (data) {
-    draw_img([course_name, prof_names, data]);
-  }
+  if (data && data.length) draw_img([course_name, prof_names, data]);
 }
--- driver.js
+++ driver.js
@@ -1,10 +1,8 @@
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 window.onload = async function() {
   if (document.location.href.match(/select_entry.php\?ACIXSTORE/)) send_data();
-  else if (document.location.href.match(/Syllabus\/.*c_key=/)) {
-    try { draw_on_syllabus(); }
-    catch(err) {}
-  } else if (window.frames.topFrame) { // course system
+  if (document.location.href.match(/Syllabus\/.*c_key=/)) draw_on_syllabus();
+  if (window.frames.topFrame) { // course system
     let cache = {};
     while (true) {
       try { recolor_list(cache); }
