07b33b30eb997b2b09a00c051174730722808535
rareone0602 <rareone0602@gmail.com>
2021-02-28 13:57:16 +0800

    debug on presentation/main.js

--- app/src/content_script/nthu/login/main.js
+++ app/src/content_script/nthu/login/main.js
@@ -5,6 +5,5 @@ canvas.width = img.clientWidth;
 canvas.height = img.clientHeight;
 canvas.getContext('2d').drawImage(img, 0, 0);
 let DataURL = canvas.toDataURL('image/PNG');
-console.log(DataURL);
 fetchMessage("decaptcha", DataURL.split(',')[1]).
 then(response => document.querySelector('input[name="passwd2"]').value = response.decaptcha)
--- app/src/content_script/nthu/presentation/main.js
+++ app/src/content_script/nthu/presentation/main.js
@@ -1,16 +1,18 @@
 'use strict';
 
 let rows = getAllRows(document);
-let courses = rows.map(parseRow).map(s => s.substr(5));
+let courses = rows.map(parseRow).filter(s=>s).map(s => s.substr(5));
 let input = {university: "NTHU", courses};
 
 loadCourses(input).
 then(courseInfo => {
   rows.forEach(row => {
-    let course_number = parseRow(row).substr(5);
-    if (Object.values(courseInfo[course_number]).some(info => info.average)) {
-      row.lastElementChild.firstElementChild.style.backgroundColor = 'green';
-    }
+    try {
+      let course_number = parseRow(row).substr(5);
+      if (Object.values(courseInfo[course_number]).some(info => info.average)) {
+        row.lastElementChild.querySelector('[type="button"]').style.backgroundColor = 'green';
+      }
+    } catch(e) {}
   });
   insertListenerToDom(courseInfo, document);
 });
